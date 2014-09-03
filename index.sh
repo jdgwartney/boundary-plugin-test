281f29c11f64d1f221da7504ee8789207616ddb6
jdgwartney <davidg@boundary.com>
2014-09-03 12:56:48 -0700

    Log date and pid to standard error

--- index.sh
+++ index.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "$1 $2 $3" >&2
+echo "$(date) pid=$$ $1 $2 $3" >&2
 echo "TEST_METRIC $RANDOM $(hostname)"
 sleep $(($2 / 1000))
-
-echo "$(date) pid=$$" >> /var/log/plugin-test.log
